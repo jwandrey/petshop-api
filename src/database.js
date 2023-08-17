471ff74149059fbcf7350826c809875ff1fee291
isadfrn <isadfrn@gmail.com>
2023-08-17 19:27:39 -0300

    feat: Add base project

---
+++ src/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  petshop: {
+    name: "The King Cat Store",
+    city: "London",
+    username: "admin-tkcs",
+    password: "wJ5EC@Z@&$*@nY6#",
+  },
+  clients: [],
+  pets: [],
+  products: [],
+  orders: [],
+};
