2f378c890fbb980fc25f9d536fed4db78835c5e1
isadfrn <isadfrn@gmail.com>
2023-08-24 17:51:11 -0300

    feat: Add cors and try..catch to all users routes

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -2,79 +2,103 @@ const database = require("../database");
 let id = 1;
 
 const create = (request, response) => {
-  const { name, email, password, endereco } = request.body;
-  const newClient = { id, name, email, password, endereco };
-  database.clients.push(newClient);
-  id++;
-  return response.status(201).send({ message: "Client created successfully" });
+  try {
+    const { name, email, password, endereco } = request.body;
+    const newClient = { id, name, email, password, endereco };
+    database.clients.push(newClient);
+    id++;
+    return response
+      .status(201)
+      .send({ message: "Client created successfully" });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const read = (request, response) => {
-  const findedClient = database.clients;
-  return response.status(200).send(findedClient);
+  try {
+    const findedClient = database.clients;
+    return response.status(200).send(findedClient);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const readById = (request, response) => {
-  const { id } = request.params;
-  const findedClientById = database.clients.find(
-    (client) => client.id === Number(id)
-  );
+  try {
+    const { id } = request.params;
+    const findedClientById = database.clients.find(
+      (client) => client.id === Number(id)
+    );
 
-  if (!findedClientById) {
-    return response.status(404).send({ message: "Client not found" });
+    if (!findedClientById) {
+      return response.status(404).send({ message: "Client not found" });
+    }
+
+    return response.status(200).send(findedClientById);
+  } catch (error) {
+    console.error(error.message);
   }
-
-  return response.status(200).send(findedClientById);
 };
 
 const update = (request, response) => {
-  const { id } = request.params;
-  const { name, email, password, endereco } = request.body;
-  const findedClientById = database.clients.find(
-    (client) => client.id === Number(id)
-  );
+  try {
+    const { id } = request.params;
+    const { name, email, password, endereco } = request.body;
+    const findedClientById = database.clients.find(
+      (client) => client.id === Number(id)
+    );
 
-  if (!findedClientById) {
-    return response.status(404).send({ message: "Client not found" });
+    if (!findedClientById) {
+      return response.status(404).send({ message: "Client not found" });
+    }
+
+    const findedClientIndex = database.clients.findIndex((object) => {
+      return object.id === Number(id);
+    });
+
+    if (findedClientIndex !== -1) {
+      database.clients[findedClientIndex] = {
+        id: Number(id),
+        name,
+        email,
+        password,
+        endereco,
+      };
+    }
+
+    return response.status(200).send(database.clients[findedClientIndex]);
+  } catch (error) {
+    console.error(error.message);
   }
-
-  const findedClientIndex = database.clients.findIndex((object) => {
-    return object.id === Number(id);
-  });
-
-  if (findedClientIndex !== -1) {
-    database.clients[findedClientIndex] = {
-      id: Number(id),
-      name,
-      email,
-      password,
-      endereco,
-    };
-  }
-
-  return response.status(200).send(database.clients[findedClientIndex]);
 };
 
 const remove = (request, response) => {
-  const { id } = request.params;
+  try {
+    const { id } = request.params;
 
-  const findedClientById = database.clients.find(
-    (client) => client.id === Number(id)
-  );
+    const findedClientById = database.clients.find(
+      (client) => client.id === Number(id)
+    );
 
-  if (!findedClientById) {
-    return response.status(404).send({ message: "Client not found" });
+    if (!findedClientById) {
+      return response.status(404).send({ message: "Client not found" });
+    }
+
+    const findedClientIndex = database.clients.findIndex((object) => {
+      return object.id === Number(id);
+    });
+
+    if (findedClientIndex !== -1) {
+      database.clients.splice(findedClientIndex, 1);
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Client deleted successfully" });
+  } catch (error) {
+    console.error(error.message);
   }
-
-  const findedClientIndex = database.clients.findIndex((object) => {
-    return object.id === Number(id);
-  });
-
-  if (findedClientIndex !== -1) {
-    database.clients.splice(findedClientIndex, 1);
-  }
-
-  return response.status(200).send({ message: "Client deleted successfully" });
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const clientsController = require("./controllers/clientsController");
 
 const app = express();
-const port = 3000;
+const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 
-// user routes
+/**
+ * * User routes
+ */
 app.post("/user", clientsController.create);
 app.get("/user", clientsController.read);
 app.get("/user/:id", clientsController.readById);
